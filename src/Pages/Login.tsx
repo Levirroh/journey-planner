754f53297f271aa0e0fcfb7592cb45d8e263c2ee
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-05 20:39:18 -0300

    feat: login page sketch

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,6 +4,10 @@ function Login() {
   return (
     <div>
       <h1>PacknGo</h1>    
+      <label htmlFor="username">Nome de usuário ou email:</label>
+      <input name="username"></input>
+      <label htmlFor="password">Senha:</label>
+      <input name="password"></input>
       <button><a href="">Entrar</a></button>    
     </div>
   )
