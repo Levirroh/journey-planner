f6b1a69fff1e4ddfecef767438be63f474e715c9
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-07 21:38:09 -0300

    feat: proper Welcome page "kinda"

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,11 +1,20 @@
+import Button from "../Components/Button.tsx"
 
 function Welcome() {
-
   return (
-    <div>
-      <h1>PacknGo</h1>    
-      <button><a href="login">Entrar</a></button>    
-      <button><a href="register">Cadastre-se</a></button>    
+    <div className="h-screen w-screen flex flex-col">
+      <div className="h-1/2 w-full bg-slate-200 flex justify-center items-center">
+        <h1 className="text-4xl font-bold">PacknGo</h1>
+      </div>
+
+      <div className="h-1/2 w-full bg-blue-400 flex justify-evenly items-center">
+        <div>
+          <Button text="Entrar" />
+        </div>
+        <div>
+          <Button text="Cadastrar" />
+        </div>
+      </div>
     </div>
   )
 }
