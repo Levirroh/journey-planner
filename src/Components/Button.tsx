a6546f26c956bd792b1401fc1a37dd9038895698
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-11 23:21:22 -0300

    feat: button component

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,14 +1,28 @@
 type ButtonProps = {
   text: string
   color?: string
+  height?: string
+  width?: string
+  hoverColor?: string
+  href?: string
 }
 
-function Button({ text, color = "bg-white" }: ButtonProps) {
-  return (
-    <button className={`${color} text-center border-none rounded-2xl px-4 py-2`}>
-      {text}
-    </button>
-  )
+function Button({ text, color = "bg-white", height = "h-15", width = "w-25", hoverColor = "bg-blue-500", href = "" }: ButtonProps) {
+  if(href != ""){
+    return(
+      <button className={`${color} text-center ${height} ${width} rounded-2xl px-4 py-2 hover:${hoverColor} hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
+        <a href={href}>
+          {text}
+        </a>
+      </button>
+    );
+  } else {
+    return(
+      <button className={`${color} text-center ${height} ${width} rounded-2xl px-4 py-2 hover:${hoverColor} hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
+          {text}
+      </button>
+    );
+  }
 }
 
 export default Button
