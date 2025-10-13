eb8d0b0b7402597e7e3e92217009ee39f6a0c260
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-12 21:43:15 -0300

    style: login page minor styles

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,17 +4,17 @@ function Login() {
 
   return (
     <div className="h-screen w-screen flex flex-col items-center">
-      <div className="h-2/5 bg-blue-400 w-full">
+      <div className="h-2/5 flex bg-blue-400 w-full items-center justify-center font-bold text-white text-6xl">
         <h1>PacknGo</h1>    
       </div>
       <div className="h-3/5 bg-slate-200 w-full flex flex-col items-center justify-evenly">
         <div className="flex flex-col">
           <label htmlFor="username" className="pl-1 text-lg">Email:</label>
-          <input name="username" className="bg-slate-300 border-2 border-slate-400 rounded-md p-1"></input>
+          <input name="username" className="bg-slate-300 border-2 border-slate-400 rounded-md p-1 text-slate-800"></input>
         </div>
         <div className="flex flex-col">
           <label htmlFor="password" className="pl-1 text-lg">Senha:</label>
-          <input name="password" className="bg-slate-300 border-2 border-slate-400 rounded-md p-1"></input>
+          <input name="password" className="bg-slate-300 border-2 border-slate-400 rounded-md p-1 text-slate-800 "></input>
         </div>
         <div>
           <Button text="Entrar" color="bg-slate-300" hoverColor="bg-blue-400"/>    
