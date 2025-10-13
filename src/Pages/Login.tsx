95209eea00b640abb3d0dd41f0eb2349c655457b
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-12 21:39:58 -0300

    feat: basic login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,14 +1,25 @@
+import Button from "../Components/Button"
 
 function Login() {
 
   return (
-    <div>
-      <h1>PacknGo</h1>    
-      <label htmlFor="username">Nome de usuário ou email:</label>
-      <input name="username"></input>
-      <label htmlFor="password">Senha:</label>
-      <input name="password"></input>
-      <button><a href="">Entrar</a></button>    
+    <div className="h-screen w-screen flex flex-col items-center">
+      <div className="h-2/5 bg-blue-400 w-full">
+        <h1>PacknGo</h1>    
+      </div>
+      <div className="h-3/5 bg-slate-200 w-full flex flex-col items-center justify-evenly">
+        <div className="flex flex-col">
+          <label htmlFor="username" className="pl-1 text-lg">Email:</label>
+          <input name="username" className="bg-slate-300 border-2 border-slate-400 rounded-md p-1"></input>
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="password" className="pl-1 text-lg">Senha:</label>
+          <input name="password" className="bg-slate-300 border-2 border-slate-400 rounded-md p-1"></input>
+        </div>
+        <div>
+          <Button text="Entrar" color="bg-slate-300" hoverColor="bg-blue-400"/>    
+        </div>
+      </div>
     </div>
   )
 }
