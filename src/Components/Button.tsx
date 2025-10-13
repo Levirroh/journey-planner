9c4d2526c97213bff34c7171bf36e544edf7dde3
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-12 21:39:48 -0300

    fix: links & hover-color

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -8,13 +8,14 @@ type ButtonProps = {
 }
 
 function Button({ text, color = "bg-white", height = "h-15", width = "w-25", hoverColor = "bg-blue-500", href = "" }: ButtonProps) {
+
   if(href != ""){
     return(
-      <button className={`${color} text-center ${height} ${width} rounded-2xl px-4 py-2 hover:${hoverColor} hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
-        <a href={href}>
+      <a href={href}>
+        <button className={`${color} text-center ${height} ${width} rounded-2xl px-4 py-2 hover:${hoverColor} hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
           {text}
-        </a>
-      </button>
+        </button>
+      </a>
     );
   } else {
     return(
