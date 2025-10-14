56e0945a751705aecf6cc30db46cbe57d33f6453
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-13 22:03:40 -0300

    feat: basic register page

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,71 +1,105 @@
 
 function Register() {
 
+  var page = 0;
+
+  function changePage(operation: string) {
+    if (operation == "return") page--;
+    else if (operation == "next") page++
+
+  }
+
   return (
-    <div>
-      <h1>PacknGo</h1>
+    <div className="h-screen w-screen flex flex-col items-center">
+      <div className="h-2/5 flex bg-blue-400 w-full items-center justify-center font-bold text-white text-6xl">
+        <h1>PacknGo</h1>
+      </div>
+      <div className="h-3/5 bg-slate-200 w-full flex flex-col items-center justify-evenly">
+        {page == 0 && (
+          <div>
+            <div>
+              <h2>Crie sua conta</h2>
+              <label>Nome completo:</label>
+              <input name="fullName" />
 
-      <h2>Crie sua conta</h2>
-      <label>Nome completo:</label>
-      <input name="fullName" />
+              <label>Como gostaria de ser chamado?</label>
+              <input name="nickname" />
 
-      <label>Como gostaria de ser chamado?</label>
-      <input name="nickname" />
+              <label>Email:</label>
+              <input name="email" type="email" />
 
-      <label>Email:</label>
-      <input name="email" type="email" />
+              <label>Telefone (opcional):</label>
+              <input name="phone" type="tel" />
 
-      <label>Telefone (opcional):</label>
-      <input name="phone" type="tel" />
+              <label>Data de nascimento:</label>
+              <input name="birthdate" type="date" />
+            </div>
+          </div>
+        )}
+        {page == 1 && (
+          <div>
+            <label>País:</label>
+            <input name="country" />
 
-      <label>Data de nascimento:</label>
-      <input name="birthdate" type="date" />
+            <label>Estado:</label>
+            <input name="state" />
 
-      <label>País:</label>
-      <input name="country" />
+            <label>Cidade:</label>
+            <input name="city" />
 
-      <label>Estado:</label>
-      <input name="state" />
+            <label>Idioma preferido:</label>
+            <select name="language">
+              <option>Português</option>
+              <option>Inglês</option>
+              <option>Espanhol</option>
+            </select>
 
-      <label>Cidade:</label>
-      <input name="city" />
+            <label>Moeda preferida:</label>
+            <select name="currency">
+              <option>BRL (R$)</option>
+              <option>USD ($)</option>
+              <option>EUR (€)</option>
+            </select>
+          </div>
+        )}
+        {page == 2 && (
+          <div>
+            <h2>Preferências de viagem (opcional)</h2>
+            <label>Três países que gostaria de visitar:</label>
+            <select name="wishCountry1"></select>
+            <select name="wishCountry2"></select>
+            <select name="wishCountry3"></select>
 
-      <label>Idioma preferido:</label>
-      <select name="language">
-        <option>Português</option>
-        <option>Inglês</option>
-        <option>Espanhol</option>
-      </select>
+            <label>Tipo de viagem preferido:</label>
+            <select name="travelType">
+              <option>Lazer</option>
+              <option>Negócios</option>
+              <option>Aventura</option>
+              <option>Cultural</option>
+              <option>Gastronômica</option>
+            </select>
+          </div>
+        )}
 
-      <label>Moeda preferida:</label>
-      <select name="currency">
-        <option>BRL (R$)</option>
-        <option>USD ($)</option>
-        <option>EUR (€)</option>
-      </select>
+        {page == 3 && (
+          <div>
+            <label>Senha:</label>
+            <input name="password" type="password" />
 
-      <label>Senha:</label>
-      <input name="password" type="password" />
-
-      <label>Confirmar senha:</label>
-      <input name="confirmPassword" type="password" />
-
-      <h2>Preferências de viagem (opcional)</h2>
-      <label>Três países que gostaria de visitar:</label>
-      <select name="wishCountry1"></select>
-      <select name="wishCountry2"></select>
-      <select name="wishCountry3"></select>
-
-      <label>Tipo de viagem preferido:</label>
-      <select name="travelType">
-        <option>Lazer</option>
-        <option>Negócios</option>
-        <option>Aventura</option>
-        <option>Cultural</option>
-        <option>Gastronômica</option>
-      </select>
-
-      <button>Confirmar cadastro</button>
+            <label>Confirmar senha:</label>
+            <input name="confirmPassword" type="password" />
+          </div>
+        )}
+        <div>
+          <button onClick={() => changePage("previous")}>Previous</button>
+          {page == 3 && (
+            <button>Confirm</button>
+          )}
+          {page != 3 && (
+            <button onClick={() => changePage("next")}>Next</button>
+          )}
+        </div>
+      </div>
 
     </div>
   )
