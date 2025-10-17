f3dc326d67cb95f54f473928b9510771c729e5eb
Levirroh <johann.gr14@gmail.com>
2025-10-17 19:49:58 -0300

    feat: organizing & style class

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -7,20 +7,20 @@ type ButtonProps = {
   href?: string
 }
 
-function Button({ text, color = "bg-white", height = "h-15", width = "w-25", hoverColor = "bg-blue-500", href = "" }: ButtonProps) {
+function Button({ text, color = "bg-white", height = "h-15", width = "w-25", href = "", hoverColor = "bg-blue-500" }: ButtonProps) {
 
-  if(href != ""){
-    return(
+  if (href != "") {
+    return (
       <a href={href}>
-        <button className={`${color} text-center ${height} ${width} rounded-2xl px-4 py-2 hover:${hoverColor} hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
+        <button className={`${color} hover:${hoverColor} ${height} ${width} text-center rounded-2xl px-4 py-2 hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
           {text}
         </button>
       </a>
     );
   } else {
-    return(
+    return (
       <button className={`${color} text-center ${height} ${width} rounded-2xl px-4 py-2 hover:${hoverColor} hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
-          {text}
+        {text}
       </button>
     );
   }
