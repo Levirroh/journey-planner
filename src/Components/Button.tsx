c26cddec5e1569d4b2899436a2b9df6660d499b2
Levirroh <johann.gr14@gmail.com>
2025-10-17 20:09:12 -0300

    refacture: onclick, href & basic button

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -5,24 +5,25 @@ type ButtonProps = {
   width?: string
   hoverColor?: string
   href?: string
+  onClick?: any
 }
 
-function Button({ text, color = "bg-white", height = "h-15", width = "w-25", href = "", hoverColor = "bg-blue-500" }: ButtonProps) {
+function Button({ text, color = "bg-white", height = "h-15", width = "w-25", href = "", hoverColor = "bg-blue-500", onClick = null }: ButtonProps) {
+
+  var button = (
+    <button className={`${color} hover:${hoverColor} ${height} ${width} text-center rounded-2xl px-4 py-2 hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
+      {text}
+    </button>
+  )
 
   if (href != "") {
     return (
-      <a href={href}>
-        <button className={`${color} hover:${hoverColor} ${height} ${width} text-center rounded-2xl px-4 py-2 hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
-          {text}
-        </button>
-      </a>
+      <a href={href}>{button}</a>
     );
+  } else if (onClick != null) {
+    return button;
   } else {
-    return (
-      <button className={`${color} text-center ${height} ${width} rounded-2xl px-4 py-2 hover:${hoverColor} hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
-        {text}
-      </button>
-    );
+    return button;
   }
 }
 
