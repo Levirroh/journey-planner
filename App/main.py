a394ca2268836680cfcc788a10927653410c003c
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-18 20:51:11 -0300

    fix: python & added python cache to gitignore

--- App/main.py
+++ App/main.py
@@ -1,29 +1,7 @@
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 
-from .dependencies import get_query_token, get_token_header
+app = FastAPI()
 
-from .routers.users import users
-## or from .routers import users  #this will import everything, that can create a colision with names, e.g. the above especifies
-
-app = FastAPI(dependencies=[Depends(get_query_token)])
-
-app.include_router(users.router)
-# e. g. app.include_router(items.router) ...
-# app.include_router(
-#     admin.router,
-#     prefix="/admin",
-#     tags=["admin"],
-#     dependencies=[Depends(get_token_header)],
-#     responses={418: {"description": "I'm a teapot"}},
-# )
-
-
-'''
-@app.post   : to create data.
-@app.get    : to read data.
-@app.put    : to update data.
-@app.delete : to delete data.
-'''
 
 @app.get("/")
 async def root():
