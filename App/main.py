0f2fd38be6a14168a7c2d2849b7b9ff22fe6923e
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-18 21:31:38 -0300

    feat: added CORS & router on main.py

--- App/main.py
+++ App/main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from .dependencies import get_query_token, get_token_header
+from .internal import admin
+from .routers import items, users
 
-app = FastAPI()
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+router = APIRouter()
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(users.router)
+app.include_router(items.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
+
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Bigger Applications!"}
