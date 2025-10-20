93af3503831e27d9f705a0ff1cb0afa86d52ba80
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-19 22:56:27 -0300

    feat: new basic component navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+
+        <div className="h-1/8 w-screen flex flex-col">
+            <a href="/welcome"><button>Sair</button></a>
+            <a href="/menu"><button>Menu</button></a>
+        </div>
+    )
+}
+export default Navbar;
