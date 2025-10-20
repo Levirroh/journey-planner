0446c6e0106f0f196e2aebb77345c9a6d49de1ec
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-19 22:56:15 -0300

    feat: added new page to router on frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
-import {createRoot} from "react-dom/client";
-import { createBrowserRouter, RouterProvider  } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import './index.css'
 
@@ -8,21 +8,24 @@ import App from './App.tsx'
 import Welcome from './Pages/Welcome.tsx'
 import Register from './Pages/Register.tsx'
 import Login from './Pages/Login.tsx'
+import Menu from './Pages/Menu.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>,
+    element: <App />,
     children: [
-      {index: true, element: <Welcome/>},
-      {path: "/register", element: <Register/>},
-      {path: "/login", element: <Login/>},
+      { index: true, element: <Welcome /> },
+      { path: "/welcome", element: <Welcome /> },
+      { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> },
+      { path: "/menu", element: <Menu /> },
     ]
   }
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
