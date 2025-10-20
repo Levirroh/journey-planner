eb8a25a3ab24ac6a28d1a3dd4e943431e2f85fb2
Levirroh <johann.gr14@gmail.com>
2025-10-20 08:45:02 -0300

    feat: added icon to button

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,18 +1,26 @@
 type ButtonProps = {
-  text: string
+  text?: string
   color?: string
   height?: string
   width?: string
   hoverColor?: string
   href?: string
   onClick?: any
+  icon?: string
 }
 
-function Button({ text, color = "bg-white", height = "h-15", width = "w-25", href = "", hoverColor = "bg-blue-500", onClick = null }: ButtonProps) {
+function Button({ text = "", color = "bg-white", height = "h-15", width = "w-25", href = "", hoverColor = "bg-blue-500", onClick = null, icon = "" }: ButtonProps) {
 
   var button = (
     <button className={`${color} hover:${hoverColor} ${height} ${width} text-center rounded-2xl px-4 py-2 hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
-      {text}
+      {text != "" && (
+        <div>
+          {text}
+        </div>
+      )}
+      {icon != "" && (
+        <img src={icon} alt={icon} />
+      )}
     </button>
   )
 
