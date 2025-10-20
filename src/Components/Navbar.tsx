ee5a1ad7c30d8dee583bfc790f2dee0bd5c6cf65
Levirroh <johann.gr14@gmail.com>
2025-10-20 10:43:48 -0300

    style: hidden navbar when screen is on pc

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 function Navbar() {
     return (
-        <div className="h-1/8 w-screen flex flex-col bg-blue-400 justify-between">
+        <div className="h-1/8 w-screen flex flex-col bg-blue-400 justify-between md:hidden block">
             <a href="/menu"><button>Menu</button></a>
             <a href="/welcome"><button>Sair</button></a>
         </div>
