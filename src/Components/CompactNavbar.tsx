7e53e1727f6f9b61d218150bcc32fdc581df77cf
Levirroh <johann.gr14@gmail.com>
2025-10-20 08:45:15 -0300

    feat: added navbar to mobile

---
+++ src/Components/CompactNavbar.tsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+function Navbar() {
+    return (
+        <div className="h-1/8 w-screen flex flex-col bg-blue-400 justify-between">
+            <Button icon="" width="h-15"/>
+            <Button icon="" width="h-15"/>
+            <Button icon="../Images/plane.svg" width="h-15"/>
+            <Button icon="" width="h-15"/>
+            <Button icon="" width="h-15"/>
+        </div>
+    )
+}
+export default Navbar;
