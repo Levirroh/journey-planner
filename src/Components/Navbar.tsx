8549f2c7fb71084b40d932b90a464c75c1648933
Levirroh <johann.gr14@gmail.com>
2025-10-20 08:45:32 -0300

    feat: organizing navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,8 @@
 function Navbar() {
     return (
-
-        <div className="h-1/8 w-screen flex flex-col">
-            <a href="/welcome"><button>Sair</button></a>
+        <div className="h-1/8 w-screen flex flex-col bg-blue-400 justify-between">
             <a href="/menu"><button>Menu</button></a>
+            <a href="/welcome"><button>Sair</button></a>
         </div>
     )
 }
