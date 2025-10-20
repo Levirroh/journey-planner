6ba64f24f723dc50604bfdcb3eca5ff392264abb
Levirroh <johann.gr14@gmail.com>
2025-10-20 10:43:33 -0300

    fix: Navbar to CompactNavbar

--- src/Components/CompactNavbar.tsx
+++ src/Components/CompactNavbar.tsx
@@ -1,8 +1,8 @@
 import Button from "./Button";
 
-function Navbar() {
+function CompactNavbar() {
     return (
-        <div className="h-1/8 w-screen flex flex-col bg-blue-400 justify-between">
+        <div className="h-1/8 w-screen flex flex-col bg-blue-400 justify-between md:block hidden">
             <Button icon="" width="h-15"/>
             <Button icon="" width="h-15"/>
             <Button icon="../Images/plane.svg" width="h-15"/>
@@ -11,4 +11,4 @@ function Navbar() {
         </div>
     )
 }
-export default Navbar;
+export default CompactNavbar;
