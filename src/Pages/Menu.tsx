326e89f84ddd68827923c1c13da0ff1f216cef33
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-21 22:16:25 -0300

    minor changes

--- src/Pages/Menu.tsx
+++ src/Pages/Menu.tsx
@@ -1,13 +1,12 @@
 import Navbar from "../Components/Navbar"
 import CompactNavbar from "../Components/CompactNavbar"
-
 function Menu() {
 
   return (
     <div>
       <Navbar />
       <section>
-        
+
       </section>
       <CompactNavbar />
     </div>
