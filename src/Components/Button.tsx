22384d3fa07c6680506282d38b65afbbf138978a
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-21 22:15:49 -0300

    style: improved classes

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -12,7 +12,7 @@ type ButtonProps = {
 function Button({ text = "", color = "bg-white", height = "h-15", width = "w-25", href = "", hoverColor = "bg-blue-500", onClick = null, icon = "" }: ButtonProps) {
 
   var button = (
-    <button className={`${color} hover:${hoverColor} ${height} ${width} text-center rounded-2xl px-4 py-2 hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer`}>
+    <button className={`${color} hover:${hoverColor} ${height} ${width} text-center rounded-2xl px-4 py-2 hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-40 hover:border-white hover:cursor-pointer flex justify-center items-center`}>
       {text != "" && (
         <div>
           {text}
