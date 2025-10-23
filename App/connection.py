dec1e99c4b7c9bd5fd85e2e3da10b11db6278094
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-22 23:29:27 -0300

    feat: connection with mysql

--- App/connection.py
+++ App/connection.py
@@ -1,31 +1,15 @@
 import mysql.connector
+from mysql.connector import Error
 
-try:
-    conn = mysql.connector.connect(
-        host="localhost",  
-        user="root",
-        password="root",
-        database="journey_planner"
-    )
-    print("Conexão bem-sucedida!")
-
-    # Crie um cursor para executar comandos
-    cursor = conn.cursor()
-
-    # Exemplo: Execute um comando SQL (substitua por seu comando)
-    cursor.execute("SELECT * FROM users")
-
-    # Exiba os resultados
-    for (col) in cursor:
-        print(f"{col}")
-
-except mysql.connector.Error as err:
-    print(f"Erro ao conectar: {err}")
-
-finally:
-    # Feche o cursor e a conexão
-    if 'cursor' in locals() and cursor is not None:
-        cursor.close()
-    if 'conn' in locals() and conn is not None and conn.is_connected():
-        conn.close()
-        print("Conexão com o MySQL fechada.")
+def get_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root", 
+            password="root",
+            database="journey_planner"
+        )
+        return connection
+    except Error as e:
+        print(f"Erro ao conectar ao MySQL: {e}")
+        return None
