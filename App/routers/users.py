61b919f6daeacb6c2a35e61714794971d080a7c8
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-10-22 23:30:06 -0300

    feat: users router w/ connection with mysql

--- App/routers/users.py
+++ App/routers/users.py
@@ -1,27 +1,54 @@
-from fastapi import APIRouter, Depends, HTTPException
-from ..dependencies import get_token_header
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from ..connection import get_connection
+import bcrypt
+
 
 router = APIRouter(
     prefix="/users",
-    tags=["users"],
-    dependencies=[Depends(get_token_header)],
-    responses={404: {"description": "Not found"}},
+    tags=["users"]
 )
 
+#region Classes
+class LoginRequest(BaseModel):
+    nickname: str
+    password: str
+
+#endregion
+
+@router.post("/login")
+def login_user(request: LoginRequest):
+    conn = get_connection()
+    if conn is None:
+        raise HTTPException(status_code=500, detail="Erro de conexão com o banco")
+
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users WHERE user_nickname = %s", (request.nickname,))
+    user = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Usuário não encontrado")
+
+    if bcrypt.checkpw(request.password.encode("utf-8"), user["user_password"].encode("utf-8")):
+        return {"message": "Login bem-sucedido", "user_id": user["user_id"]}
+    else:
+        raise {"message": "usuário inválido"}
 
 
 
+@router.get("/")
+def get_users():
+    conn = get_connection()
+    if conn is None:
+        return {"error": "Não foi possível conectar ao banco de dados."}
 
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users")
+    result = cursor.fetchall()
 
-
-@router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
-
-
-@router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
+    cursor.close()
+    conn.close()
+    return result
