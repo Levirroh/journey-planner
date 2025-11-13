b18477b6c19786201dee61c010d68e7785eb840b
Levirroh <johann.gr14@gmail.com>
2025-11-13 14:58:45 -0300

    feat: added arrow component to menu

--- src/Pages/Menu.tsx
+++ src/Pages/Menu.tsx
@@ -1,10 +1,13 @@
 import Navbar from "../Components/Navbar"
 import CompactNavbar from "../Components/CompactNavbar"
+import Arrow from "../Components/Arrow"
+
 function Menu() {
 
   return (
     <div>
       <Navbar />
+      <Arrow text="Voltar" href="/" />
       <section>
 
       </section>
