1627334cfee701f3c54c25f712642532ac33f941
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-15 11:36:10 -0300

    feat: register user

--- App/routers/users.py
+++ App/routers/users.py
@@ -14,6 +14,21 @@ class LoginRequest(BaseModel):
     nickname: str
     password: str
 
+class RegisterRequest(BaseModel):
+    email: str
+    nickname: str
+    phone: str
+    birth: str
+    country: str
+    state: str
+    city: str
+    password: str
+    wishCountryOne: str
+    wishCountryTwo: str
+    wishCountryThree: str
+    travelType: str
+
+
 #endregion
 
 @router.post("/login")
@@ -37,7 +52,29 @@ async def login_user(request: LoginRequest):
     else:
         raise {"message": "usuário inválido"}
 
+@router.post("/register")
+async def login_user(request: RegisterRequest):
+    conn = get_connection()
+    if conn is None:
+        raise HTTPException(status_code=500, detail="Erro de conexão com o banco")
 
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("INSERT INTO users (user_email, user_nickname, user_phone, user_birth, user_country, user_state, user_city, user_password, wish_country_one, wish_country_two, wish_country_three, travel_type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+                    (request.email, request.nickname, request.phone, request.birth, request.country, request.state, request.city, 
+                    bcrypt.hashpw(request.password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8"), 
+                    request.wishCountryOne, request.wishCountryTwo, request.wishCountryThree, request.travelType))
+    conn.commit()
+    cursor.execute("SELECT * FROM users WHERE user_email = %s OR user_nickname = %s", (request.nickname, request.nickname))
+    user = cursor.fetchone()
+
+
+    cursor.close()
+    conn.close()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Usuário não cadastrado")
+    else:
+        return {"message": "Registro bem-sucedido", "user_id": user["user_id"]}
 
 @router.get("/")
 def get_users():
