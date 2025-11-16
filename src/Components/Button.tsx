50d84456d32094f09120d04ebea1410fb0f5139f
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-16 17:05:32 -0300

    feat: new props for input

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -4,6 +4,7 @@ type ButtonProps = {
   height?: string
   width?: string
   hoverColor?: string
+  focusColor?: string
   href?: string
   onClick?: any
   icon?: string
@@ -16,6 +17,7 @@ function Button({
   width = "w-25",
   href = "",
   hoverColor = "hover:bg-blue-500",
+  focusColor = "focus:bg-blue-500",
   onClick = null,
   icon = ""
 }: ButtonProps) {
@@ -24,7 +26,15 @@ function Button({
     <button
       type="button"
       onClick={onClick}
-      className={`${color}  ${hoverColor}   ${height}  ${width} text-center rounded-2xl px-4 py-2 hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl duration-150 hover:w-50 hover:border-white hover:cursor-pointer flex justify-center items-center`}
+      className={`${color} outline-0  ${hoverColor} ${focusColor}  ${height}  ${width} 
+
+      duration-150 rounded-2xl px-4 py-2 
+
+      hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl hover:w-50 hover:border-white hover:cursor-pointer 
+
+      focus:border-2 focus:h-20 focus:duration-150 focus:text-white focus:text-2xl focus:w-50 focus:border-white 
+
+      flex justify-center items-center text-center `}
     >
       {text && <div>{text}</div>}
       {icon && <img src={icon} alt={icon} />}
