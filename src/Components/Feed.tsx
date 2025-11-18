5b14c4103017c72b2b079b497ec4b7b27a570d90
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-17 21:57:36 -0300

    feat: first idea of feed

---
+++ src/Components/Feed.tsx
@@ -0,0 +1,18 @@
+
+
+function Feed() {
+    function GetFeed() {
+
+    }
+
+
+
+
+
+    return (
+        <div className="h-full w-screen bg-slate-200 flex flex-col items-center">
+
+        </div>
+    )
+}
+export default Feed;
