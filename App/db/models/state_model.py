87e981e75aad2f4d93a25a18213dda5833b2c644
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-20 08:41:00 -0300

    feat: state_model

---
+++ App/db/models/state_model.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional
+
+class State(SQLModel, table=True):
+    code: str = Field(primary_key=True)
+    name: str
+
+    country_code: str = Field(foreign_key="country.code")
+
+    country: Optional["Country"] = Relationship(back_populates="states")
+    cities: List["City"] = Relationship(back_populates="state")
