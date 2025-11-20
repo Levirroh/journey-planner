788fb1be683b8ecc18ef70e0e7af73e9603850c4
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-20 08:41:07 -0300

    feat: user_model

---
+++ App/db/models/user_model.py
@@ -0,0 +1,23 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class User(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, primary_key=True)
+
+    user_name: str
+    user_email: str
+    user_nickname: str
+    user_phone: str
+    user_birth: str
+
+    user_country: str = Field(foreign_key="country.code")
+    user_state: str = Field(foreign_key="state.code")
+    user_city: str = Field(foreign_key="city.code")
+
+    user_password: str
+
+    wish_country_one: Optional[str] = Field(default=None, foreign_key="country.code")
+    wish_country_two: Optional[str] = Field(default=None, foreign_key="country.code")
+    wish_country_three: Optional[str] = Field(default=None, foreign_key="country.code")
+
+    travel_type: str
