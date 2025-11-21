150348df679d99581f9974c34a5f9a35a7ceb490
Levirroh <johann.gr14@gmail.com>
2025-11-21 10:22:09 -0300

    feat: new plane routes

---
+++ App/routers/plane/plane.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+import bcrypt
+
+from sqlmodel import select
+
+from App.db.database import get_session
+from App.db.models import Plane
+
+router = APIRouter(
+    prefix="/plane",
+    tags=["plane"]
+)
+
+#region Classes
+class seePlane(BaseModel):
+    plane_id: int
+
+#endregion
+
+#region Requests
+@router.post("/seePlane")
+async def see_plane(request: seePlane, session = Depends(get_session)):
+    query = select(Plane).where(
+        (Plane.plane_id == request.plane_id)
+    )
+    return query
+
+
+@router.post("/")
+async def all_plane(session = Depends(get_session)):
+    query = select(Plane).all()
+    return query
+#endregion
