59b02c1c586d509b1e95caeafc1a9f9893ae5698
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-22 15:07:01 -0300

    fix: FKs

--- App/db/models/state_model.py
+++ App/db/models/state_model.py
@@ -5,7 +5,7 @@ class States(SQLModel, table=True):
     code: str = Field(primary_key=True)
     name: str
 
-    country_code: str = Field(foreign_key="country.code")
+    country_code: str = Field(foreign_key="countries.code")
     
-    country: Optional["Country"] = Relationship(back_populates="states")
-    cities: List["City"] = Relationship(back_populates="state")
+    country: Optional["Countries"] = Relationship(back_populates="states")
+    cities: List["Cities"] = Relationship(back_populates="state")
