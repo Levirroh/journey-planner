a3054dd0a9eddb01065317282b9e0b47edd9059d
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-22 15:07:24 -0300

    fix: import names

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel
-from App.db.models import Country, State, City, User
+from App.db.models import Countries, States, Cities, Users
 
 #aplying the models' metadata for migrations
 target_metadata = SQLModel.metadata
