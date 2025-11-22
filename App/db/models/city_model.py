3789a389c22d62833ef47cdeedb4492f65bd480d
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-22 15:06:39 -0300

    fix: FKs

--- App/db/models/city_model.py
+++ App/db/models/city_model.py
@@ -5,6 +5,6 @@ class Cities(SQLModel, table=True):
     code: str = Field(primary_key=True)
     name: str
 
-    state_code: str = Field(foreign_key="state.code")
+    state_code: str = Field(foreign_key="states.code")
 
-    state: Optional["State"] = Relationship(back_populates="cities")
+    state: Optional["States"] = Relationship(back_populates="cities")
