e3b4727b71504bc9ba1e1cb6e381b19ea8a4cfc3
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-11-23 12:18:01 -0300

    syle: minor changes

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -4,6 +4,7 @@ type ButtonProps = {
   height?: string
   width?: string
   hoverColor?: string
+  focusColor?: string
   href?: string
   onClick?: any
   icon?: string
@@ -16,6 +17,7 @@ function Button({
   width = "w-25",
   href = "",
   hoverColor = "hover:bg-blue-500",
+  focusColor = "focus:bg-blue-500",
   onClick = null,
   icon = ""
 }: ButtonProps) {
@@ -24,13 +26,23 @@ function Button({
     <button
       type="button"
       onClick={onClick}
-      className={`${color} outline-0  ${hoverColor}  ${height}  ${width} 
+      className={`
+        ${color} ${hoverColor} ${focusColor} ${height} ${width}
+        px-4 py-2 rounded-2xl outline-0
 
-      duration-150 rounded-2xl px-4 py-2 
+        text-center flex justify-center items-center
+        transition-all duration-200 transform
 
-      hover:border-2 hover:h-20 hover:duration-150 hover:text-white hover:text-2xl hover:w-50 hover:border-white hover:cursor-pointer 
+        hover:scale-[1.25]
+        focus:scale-[1.25]
+        active:scale-[0.98]
 
-      flex justify-center items-center text-center `}
+        hover:shadow-lg
+        focus:shadow-lg
+        shadow-md
+
+        border border-transparent hover:border-white focus:border-white 
+      `}
     >
       {text && <div>{text}</div>}
       {icon && <img src={icon} alt={icon} />}
