2b2090fd4fef313f5c3e80fdbb45273f9670eef2
Levirroh <johann.gr14@gmail.com>
2025-11-27 16:34:53 -0300

    feat: added test button to insert data

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,8 +1,20 @@
+import axios from "axios";
 import Button from "../Components/Button.tsx"
 
 function Welcome() {
+  async function insertTestData() {
+    try {
+      await axios.get("http://localhost:8000/users/login");
+    } catch (error) {
+    }
+  }
+
+
   return (
     <div className="h-screen w-screen flex flex-col">
+
+      <button onClick={insertTestData}> inserir dados de teste </button>
+
       <div className="h-1/2 w-full bg-slate-200 flex justify-center items-center">
         <h1 className="text-4xl font-bold">PacknGo</h1>
       </div>
