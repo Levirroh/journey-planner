32f210167d2189499f7719c57b5503012d79c31a
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-09 19:28:40 -0300

    style: minor changes

--- src/Components/FlightContainer.tsx
+++ src/Components/FlightContainer.tsx
@@ -11,11 +11,11 @@ function FlightContainer({
 }: FlightContainerProps) {
 
   const flightContainer = (
-    <div className="w-85 h-45">
-      <div>
-        <p>{flight?.title}</p>
+    <div className="text-black w-85 h-45 p-4 rounded-lg flex flex-col justify-center items-center bg-gradient-to-br from-gray-300 to-gray-400 shadow-lg">
+      <div className="bg-slate-200 w-[90%] h-[30%] rounded-lg flex justify-center items-center mb-2">
       </div>
-      <div className="w-[90%] h-[60%] p-4 rounded-lg flex flex-col justify-center items-center bg-gradient-to-br from-gray-300 to-gray-400 text-white shadow-lg">
+      <div className="w-[90%] h-[60%]">
+        <p className="">{flight?.title}</p>
         <p>{flight?.departure}</p>
         <p>{flight?.arriving}</p>
       </div>
