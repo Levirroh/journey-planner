6a9ba78e93a21467fafc506a1817ead296270bb0
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-09 19:36:16 -0300

    style: minor changes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div>
       <Navbar />
-      <section>
+      <section className="w-screen">
         <div className="h-1/5 flex bg-blue-400 w-full items-center justify-center font-bold text-white relative">
           <h1 className="text-6xl">PacknGo</h1>
         </div>
