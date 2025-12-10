7c6cca177b523c50bca07cf5f6025a2015ced674
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-10 19:35:20 -0300

    feat: places & types of seat

--- App/db/models/seat_model.py
+++ App/db/models/seat_model.py
@@ -1,9 +1,26 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 
+class ClassTypes(str):
+    ECONOMY = "Economy"
+    P_ECONOMY = "Premium Economy"
+    BUSINESS = "Executive/Business"
+    FIRST_CLASS = "First Class"
+
+class SeatPlace(str):
+    WINDOWS = "Window"
+    CORRIDOR = "Corridor"
+    MIDDLE = "Middle"
+    EMERGENCY = "Emergency Exit"
+    WING = "Wing"
+    FRONT = "Front"
+
+
 class Seats(SQLModel, table=True):
     seat_id: Optional[int] = Field(default=None, primary_key=True)
     code: str
+    classname: ClassTypes
+    location: SeatPlace
 
     plane_id: Optional[int] = Field(foreign_key="planes.plane_id")
     plane: Optional["Planes"] = Relationship(back_populates="seats")
