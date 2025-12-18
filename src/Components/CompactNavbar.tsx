24568528aa09ab2a225b08beecac5ab9e2b4b33d
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-18 10:38:24 -0300

    fix: images to icons

--- src/Components/CompactNavbar.tsx
+++ src/Components/CompactNavbar.tsx
@@ -1,19 +1,15 @@
+import { Book, CircleUser, Plane, Search, Settings } from "lucide-react";
 import Button from "./Button";
-import PlaneSvg from "../Images/airplane.svg"
-import Settings from "../Images/settings.svg"
-import Profile from "../Images/profile-circle.svg"
-import Book from "../Images/book.svg"
-import Search from "../Images/search.svg"
 
 
 function CompactNavbar() {
     return (
         <div className="w-screen p-4 bg-blue-400 bottom-0 flex absolute justify-evenly md:hidden">
-            <Button icon={Settings} width="w-15" />
-            <Button icon={Book} width="w-15" />
-            <Button icon={PlaneSvg} width="w-25" />
-            <Button icon={Search} width="w-15" />
-            <Button icon={Profile} width="w-15" />
+            <Button icon={<Settings />} width="w-15" />
+            <Button icon={<Book />} width="w-15" />
+            <Button icon={<Plane />} width="w-25" />
+            <Button icon={<Search />} width="w-15" />
+            <Button icon={<CircleUser />} width="w-15" />
         </div>
     )
 }
