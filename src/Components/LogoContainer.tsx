ee0e88fce4ba6cb6b35473b0e069c87345558258
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-18 11:00:17 -0300

    fix: dinamic logo container

---
+++ src/Components/LogoContainer.tsx
@@ -0,0 +1,34 @@
+
+enum ETypeLogoContainer {
+  full = "full",
+  small = "small",
+  fullNoReturn = "fullNoReturn"
+}
+
+type logoContainerProps = {
+  type?: ETypeLogoContainer
+}
+
+
+function LogoContainer({ type = ETypeLogoContainer.full }: logoContainerProps) {
+
+
+  const logoContainer = (
+    <div className={`${type == ETypeLogoContainer.full ? "h-6/10" : type == ETypeLogoContainer.small ? "h-2/10" : "h-6/10"} w-full bg-[url('src/Images/background-dark.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center gap-1`}>
+      {type === ETypeLogoContainer.full && (
+        <div>
+          <img src="src/Images/Logo.png" className="h-[250px]" alt="" />
+        </div>
+      )}
+      <div className="flex justify-center items-center">
+        <h1 className="text-7xl font-bold text-white">Packn</h1>
+        <h1 className="text-7xl font-bold text-blue-400">G</h1>
+        <img src="src/Images/earth-full.png" className="h-12 w-12 mt-5" />
+      </div>
+    </div>
+  );
+
+  return logoContainer;
+}
+
+export default LogoContainer;
