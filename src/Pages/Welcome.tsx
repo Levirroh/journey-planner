0d73dbd947d54c84f9bfeca924c6f0f45d367b1c
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-18 11:00:36 -0300

    feat: Logo Container on Welcome page

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Button from "../Components/Button.tsx"
+import LogoContainer from "../Components/LogoContainer.tsx";
 
 function Welcome() {
   async function insertTestData() {
@@ -15,16 +16,7 @@ function Welcome() {
 
       {/* <button onClick={insertTestData}> inserir dados de teste </button> */}
 
-      <div className="h-6/10 w-full bg-[url('src/Images/background-dark.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center gap-1">
-        <div>
-          <img src="src/Images/Logo.png" className="h-[250px]" alt="" />
-        </div>
-        <div className="flex justify-center items-center">
-          <h1 className="text-7xl font-bold text-white">Packn</h1>
-          <h1 className="text-7xl font-bold text-blue-400">G</h1>
-          <img src="src/Images/earth-full.png" className="h-12 w-12 mt-5" />
-        </div>
-      </div>
+      <LogoContainer />
 
       <div className="h-4/10 w-full bg-gradient-to-br from-blue-500 to-blue-400 flex flex-col sm:justify-evenly justify-center gap-15 sm:gap-0 items-center sm:flex-row">
         <div>
