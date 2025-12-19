76ba6f4d50bdc5b819384596cfe99b4c92d19c0f
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-19 09:31:56 -0300

    feat: added size variants

--- src/Components/LogoContainer.tsx
+++ src/Components/LogoContainer.tsx
@@ -1,8 +1,10 @@
+import Arrow from "./Arrow";
 
-enum ETypeLogoContainer {
+export enum ETypeLogoContainer {
   big = "full",
   small = "small",
-  bigNoReturn = "fullNoReturn"
+  bigNoReturn = "bigNoReturn",
+  smallNoReturn = "smallNoReturn"
 }
 
 type logoContainerProps = {
@@ -12,22 +14,48 @@ type logoContainerProps = {
 
 function LogoContainer({ type = ETypeLogoContainer.big }: logoContainerProps) {
 
+  var logoContainer;
 
-  const logoContainer = (
-    <div className={`${type == ETypeLogoContainer.big ? "h-6/10" : type == ETypeLogoContainer.small ? "h-2/10" : "h-6/10"} w-full bg-[url('src/Images/background-dark.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center gap-1`}>
-      {type === ETypeLogoContainer.big && (
-        <div>
-          <img src="src/Images/Logo.png" className="h-[250px]" alt="" />
+  if (type == ETypeLogoContainer.big || type == ETypeLogoContainer.bigNoReturn) {
+    logoContainer = (
+      <div className={`h-6/10 relative w-full bg-[url('src/Images/background-dark.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center gap-1`}>
+        {type === ETypeLogoContainer.big && (
+          <div>
+            <Arrow href="/" />
+            <img src="src/Images/Logo.png" className="h-[250px]" alt="" />
+          </div>
+        )}
+        {type === ETypeLogoContainer.bigNoReturn && (
+          <div>
+            <img src="src/Images/Logo.png" className="h-[250px]" alt="" />
+          </div>
+        )}
+
+        <div className="flex justify-center items-center">
+          <h1 className="text-7xl font-bold text-white">Packn</h1>
+          <h1 className="text-7xl font-bold text-blue-400">G</h1>
+          <img src="src/Images/earth-full.png" className="h-12 w-12 mt-5" />
         </div>
-      )}
-      <div className="flex justify-center items-center">
-        <h1 className="text-7xl font-bold text-white">Packn</h1>
-        <h1 className="text-7xl font-bold text-blue-400">G</h1>
-        <img src="src/Images/earth-full.png" className="h-12 w-12 mt-5" />
       </div>
-    </div>
-  );
+    );
+  } else {
+    logoContainer = (
+      <div className={`h-3/10 relative w-full bg-[url('src/Images/background-dark.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center gap-1`}>
+        {type === ETypeLogoContainer.small && (
+          <div>
+            <Arrow href="/" size="w-13 h-10" />
+            <img src="src/Images/Logo.png" className="h-[75px] absolute top-1 right-2" alt="" />
+          </div>
+        )}
 
+        <div className="flex justify-center items-center">
+          <h1 className="text-7xl font-bold text-white">Packn</h1>
+          <h1 className="text-7xl font-bold text-blue-400">G</h1>
+          <img src="src/Images/earth-full.png" className="h-12 w-12 mt-5" />
+        </div>
+      </div>
+    )
+  };
   return logoContainer;
 }
 
