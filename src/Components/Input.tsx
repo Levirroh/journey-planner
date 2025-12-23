27e2abdf520fcf77b092b490964c6a32f9482e23
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-23 19:36:32 -0300

    feat: added new prop to input.tsx

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -16,6 +16,8 @@ type InputProps = {
   placeholder?: string
   color?: string
   value?: string
+  width?: string
+  height?: string
   options?: Array<string>
   onChange?: (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => void
 }
@@ -27,6 +29,8 @@ function Input({
   placeholder = "Placeholder",
   color = "bg-white",
   value,
+  width = "w-15",
+  height = "h-10",
   options = [],
   onChange,
 }: InputProps) {
@@ -37,14 +41,14 @@ function Input({
 
 
   const input = (
-    <div className="flex flex-col relative">
+    <div className="flex flex-col relative w-full h-full">
       <label className="pl-2 text-white">{text}</label>
 
       {(type === ETypeInput.input || type === ETypeInput.password) && (
         <input
           className={`
-            w-full h-12 px-4
-            rounded-[6px] border-2 border-slate-300
+            ${width} ${height} px-4
+            rounded-[6px] border-2 border-slate-400
             text-slate-700 ${color}
             transition-all duration-100
             transform
@@ -84,8 +88,8 @@ function Input({
       {type === ETypeInput.select && (
         <select
           className={`${color} 
-            w-full h-12 px-4
-            rounded-md border-2 border-slate-300
+            ${width} ${height} px-4
+            rounded-md border-2 border-slate-400
             text-slate-700
             transition-all duration-100
             transform
