7f318a66e534ddd7d9f61c5e9dfdfca4ff727d09
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-28 16:57:58 -0300

    update: text to english

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -20,10 +20,10 @@ function Welcome() {
 
       <div className="h-4/10 w-full bg-gradient-to-br from-blue-500 to-blue-400 flex flex-col sm:justify-evenly justify-center gap-15 sm:gap-0 items-center sm:flex-row">
         <div>
-          <Button text="Entrar" href="/login" hoverColor="hover:bg-zinc-800" hoverTextColor="hover:text-white" height="h-[70px]" width="w-[194px]" />
+          <Button text="Enter" href="/login" hoverColor="hover:bg-zinc-800" hoverTextColor="hover:text-white" height="h-[70px]" width="w-[194px]" />
         </div>
         <div>
-          <Button text="Cadastrar" href="/register" hoverColor="hover:bg-zinc-800" hoverTextColor="hover:text-white" height="h-[70px]" width="w-[194px]" />
+          <Button text="Register" href="/register" hoverColor="hover:bg-zinc-800" hoverTextColor="hover:text-white" height="h-[70px]" width="w-[194px]" />
         </div>
       </div>
     </div>
