a9974cfbbdf4d75ee8c59d7d64cbf42ef15a9879
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-12-28 16:50:35 -0300

    Update: ErrorMessage to ScreenMessage (added warning & success variations)

--- src/Components/Utils/ScreenMessage.tsx
+++ src/Components/Utils/ScreenMessage.tsx
@@ -1,38 +1,46 @@
 import { useEffect, useState } from "react"
 
-type ErrorMessageProps = {
+export enum ETypeScreenMessage {
+  ERROR,
+  SUCCESS,
+  WARNING,
+}
+
+type ScreenMessageProps = {
   trigger: number
+  type?: ETypeScreenMessage
   text?: string
   duration?: number
 }
 
-function ErrorMessage({
+function ScreenMessage({
   trigger,
+  type = ETypeScreenMessage.ERROR,
   text = "Erro ao validar dados",
   duration = 3000,
-}: ErrorMessageProps) {
+}: ScreenMessageProps) {
   const [visible, setVisible] = useState(false)
 
   useEffect(() => {
     if (trigger === 0) return
-
     setVisible(true)
-
     const timer = setTimeout(() => {
       setVisible(false)
     }, duration)
-
     return () => clearTimeout(timer)
   }, [trigger, duration])
 
+
   if (!visible) return null
+
+
 
   return (
     <div className="absolute self-center">
-      <div className="relative bg-red-400 text-white text-md top-1 rounded-2xl p-3">
+      <div className={`relative ${type === ETypeScreenMessage.ERROR ? "bg-red-400" : type === ETypeScreenMessage.SUCCESS ? "bg-green-500" : "bg-orange-400 "} text-white text-md top-1 rounded-2xl p-3`}>
         <p>{text}</p>
       </div>
     </div>
   )
 }
-export default ErrorMessage;
+export default ScreenMessage;
