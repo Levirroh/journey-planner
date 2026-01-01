379d7053a782eebd466c59ef5a7f6373bb8dc9d0
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-01 19:34:59 -0300

    update: starting mockup related changes

--- src/Components/CompactNavbar.tsx
+++ src/Components/CompactNavbar.tsx
@@ -1,14 +1,12 @@
-import { Book, CircleUser, Plane, Search, Settings } from "lucide-react";
+import { CircleUser, Plane, Settings } from "lucide-react";
 import Button from "./Button";
 
 
 function CompactNavbar() {
     return (
-        <div className="w-screen p-4 bg-blue-400 bottom-0 flex absolute justify-evenly md:hidden">
+        <div className="w-screen justify-evenly md:hidden relative">
             <Button icon={<Settings />} width="w-15" />
-            <Button icon={<Book />} width="w-15" />
             <Button icon={<Plane />} width="w-25" />
-            <Button icon={<Search />} width="w-15" />
             <Button icon={<CircleUser />} width="w-15" />
         </div>
     )
