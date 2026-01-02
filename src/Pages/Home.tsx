582a5316a7e5216681b2635758adda4f410caf13
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-02 12:13:53 -0300

    feat: added input props to home inputs

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,9 +4,13 @@ import Input from "../Components/Input"
 function Home() {
   return (
     <section className="w-screen h-screen bg-gradient-to-br from-blue-600 to-blue-400 relative">
-      <div className="pt-6 px-4">
-        <Input />
-        <Input />
+      <div className="w-full flex flex-col items-center justify-center gap-4 pt-6">
+        <Input width="w-72"
+          height="h-12"
+          placeholder="Type your next destiny!" />
+        <Input width="w-72"
+          height="h-12"
+          placeholder="Type from where you are departing!" />
       </div>
 
       <CompactNavbar />
