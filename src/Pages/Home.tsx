ac6858f13d37904b1610adae88bf95c33aa1e59e
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-03 19:14:30 -0300

    feat: added visible/invisible input according to destiny input

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,21 +1,35 @@
-import CompactNavbar from "../Components/CompactNavbar"
-import Input from "../Components/Input"
+import { useState } from "react";
+import Input from "../Components/Input";
+import CompactNavbar from "../Components/CompactNavbar";
 
 function Home() {
+  const [destiny, setDestiny] = useState("");
+  const [departure, setDeparture] = useState("");
+
+  const showDepartureInput = destiny.trim() !== "";
+
   return (
     <section className="w-screen h-screen bg-gradient-to-br from-blue-600 to-blue-400 relative">
       <div className="w-full flex flex-col items-center justify-center gap-4 pt-6">
-        <Input width="w-72"
+        <Input
+          width="w-72"
           height="h-12"
-          placeholder="Type your next destiny!" />
-        <Input width="w-72"
-          height="h-12"
-          placeholder="Type from where you are departing!" />
+          placeholder="Type your next destiny!"
+          onChange={(e) => setDestiny(e.target.value)}
+        />
+
+        {showDepartureInput && (
+          <Input
+            width="w-72"
+            height="h-12"
+            placeholder="Type from where you are departing!"
+            onChange={(e) => setDeparture(e.target.value)}
+          />
+        )}
       </div>
 
       <CompactNavbar />
     </section>
-  )
+  );
 }
-
 export default Home
