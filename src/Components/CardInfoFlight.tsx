5569f3a7711edf82d8beaceeecde77e716107de6
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-05 18:51:59 -0300

    feat: CardInfoFLight component

---
+++ src/Components/CardInfoFlight.tsx
@@ -0,0 +1,31 @@
+export enum ETypeCardInfoFlight {
+  discount,
+  avaliable,
+  trending,
+}
+
+type CardInfoFlightProps = {
+  info?: ETypeCardInfoFlight
+}
+
+function CardInfoFlight({
+  info = ETypeCardInfoFlight.avaliable
+}: CardInfoFlightProps) {
+
+  const cardInfoFlight = (
+    <div className={`${info == ETypeCardInfoFlight.avaliable ? "bg-green-600" :
+      info == ETypeCardInfoFlight.discount ? "bg-blue-600" :
+        info == ETypeCardInfoFlight.trending ? "bg-yellow-600" : "bg-gray-400"}
+    rounded-full w-12 h-12 flex items-center justify-center`}>
+      <p className="text-white font-bold text-sm">
+        {info == ETypeCardInfoFlight.avaliable ? "Avaliable" :
+          info == ETypeCardInfoFlight.discount ? "Discount" :
+            info == ETypeCardInfoFlight.trending ? "Trending" : "Info"}
+      </p>
+    </div>
+  );
+
+  return cardInfoFlight;
+}
+
+export default CardInfoFlight;
