91a7028fa6921137332cfe6fb10faa0d80fbf232
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-06 18:58:42 -0300

    feat: added sectionDivisor component

---
+++ src/Components/Utils/SectionDivisor.tsx
@@ -0,0 +1,17 @@
+import type { ETypeCardInfoFlight } from "../CardInfoFlight";
+import type { ETypeFare } from "../Classes/Flight";
+
+type SectionDivisorProps = {
+  tags?: ETypeCardInfoFlight[]
+  fare?: ETypeFare,
+  avaliableSeats?: number,
+}
+
+function SectionDivisor({ tags, fare, avaliableSeats }: SectionDivisorProps) {
+
+  return (
+    <div className="self-center bg-slate-300 p-2 w-full">
+    </div>
+  )
+}
+export default SectionDivisor;
