d07d5a198d00b4509eabb53b2b5a9076b6a10ab0
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-11 09:09:13 -0300

    feat: added new Enum and new table fields

--- App/db/models/user_model.py
+++ App/db/models/user_model.py
@@ -1,7 +1,18 @@
 from datetime import date
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from enum import Enum as PyEnum
 
+from App.db.models.city_model import Cities
+from App.db.models.country_model import Countries
+from App.db.models.state_model import States
+
+class ETravelType(str, PyEnum): # todo: make a table for travel types
+    LEISURE = "Leisure"
+    BUSINESS = "Business"
+    ADVENTURE = "Adventure"
+    CULTURE = "Culture"
+    GASTRONOMY = "Gastronomy"
 class Users(SQLModel, table=True):
     user_id: Optional[int] = Field(default=None, primary_key=True)
 
@@ -11,9 +22,12 @@ class Users(SQLModel, table=True):
     user_phone: str
     user_birth: date   
 
-    user_country: str = Field(foreign_key="countries.code")
-    user_state: str = Field(foreign_key="states.code")
-    user_city: int = Field(foreign_key="cities.code")
+    user_countryId: str = Field(foreign_key="countries.code")
+    user_country: Optional["Countries"] 
+    user_stateId: str = Field(foreign_key="states.code")
+    user_state: Optional["States"] 
+    user_cityId: int = Field(foreign_key="cities.code")
+    user_city: Optional["Cities"] 
 
     user_password: str
 
@@ -21,4 +35,4 @@ class Users(SQLModel, table=True):
     wish_country_two: Optional[str] = Field(default=None, foreign_key="countries.code")
     wish_country_three: Optional[str] = Field(default=None, foreign_key="countries.code")
 
-    travel_type: str
+    travel_type: ETravelType
