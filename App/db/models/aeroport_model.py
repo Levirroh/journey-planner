db1bba9c584984b9c301283f9f00218286d2f379
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-14 19:15:10 -0300

    feat: added special aeroport codes

--- App/db/models/aeroport_model.py
+++ App/db/models/aeroport_model.py
@@ -4,7 +4,10 @@ from typing import Optional
 class Aeroports(SQLModel, table=True):
     aeroport_id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    adress: str
+    address: str
+
+    iata_code: str
+    icao_code: Optional[str]
 
     state: str = Field(foreign_key="states.code")
     city: int = Field(foreign_key="cities.code")
