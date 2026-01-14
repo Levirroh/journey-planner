630a5a2a177423979afa23b28ba5019d0c023c24
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-14 19:16:13 -0300

    fix: seat table

--- App/db/models/seat_model.py
+++ App/db/models/seat_model.py
@@ -5,34 +5,10 @@ from typing import Optional
 
 from App.db.models.plane_model import Planes
 
-class ClassTypes(str, PyEnum):
-    ECONOMY = "Economy"
-    P_ECONOMY = "Premium Economy"
-    BUSINESS = "Executive/Business"
-    FIRST_CLASS = "First Class"
-
-class SeatPlace(str, PyEnum):
-    WINDOWS = "Window"
-    CORRIDOR = "Corridor"
-    MIDDLE = "Middle"
-    EMERGENCY = "Emergency Exit"
-    WING = "Wing"
-    FRONT = "Front"
-
 
 class Seats(SQLModel, table=True):
     seat_id: Optional[int] = Field(default=None, primary_key=True)
     code: str
-    weather: ClassTypes = Field(
-        sa_column=Column(Enum(ClassTypes, name="class_types_enum"))
-    )
-    location: SeatPlace = Field(
-        sa_column=Column(Enum(SeatPlace, name="seat_place_enum"))
-    )
-    location_especify: SeatPlace = Field(
-        sa_column=Column(Enum(SeatPlace, name="seat_place_especify_enum"))
-    )
+    location: str
 
     plane_id: Optional[int] = Field(foreign_key="planes.plane_id")
-    plane: Optional["Planes"] = Relationship(back_populates="seats")
-    user_id: Optional[int] = Field(foreign_key="users.user_id")
