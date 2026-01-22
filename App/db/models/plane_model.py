46cd5f91d0f5ed2810d9ba2b7225a85646163e40
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-21 21:56:42 -0300

    minor changes

--- App/db/models/plane_model.py
+++ App/db/models/plane_model.py
@@ -1,11 +1,10 @@
+from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
-from typing import List, Optional
 
-from App.db.models.seat_model import Seats
 
 class Planes(SQLModel, table=True):
     plane_id: Optional[int] = Field(default=None, primary_key=True)
     model: str
     total_seats: int
 
-    seats: List["Seats"] = Relationship(back_populates="plane")
+    flights: List["Flights"] = Relationship(back_populates="plane")
