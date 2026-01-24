acf7fd98aa5e219d8e5e0827db31f29dca0725c8
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-01-24 09:45:18 -0300

    feat: on welome page, insert and delete base data functions can be called

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,10 +1,27 @@
+import axios from "axios";
 import Button from "../Components/Button.tsx"
 import LogoContainer, { ETypeLogoContainer } from "../Components/LogoContainer.tsx";
 
 function Welcome() {
 
+  async function insertTestData() {
+    try {
+      await axios.get("http://localhost:8000/general/insertBaseData");
+    } catch (error) {
+    }
+  }
+
+  async function dropBaseData() {
+    try {
+      await axios.delete("http://localhost:8000/general/dropBaseData");
+    } catch (error) {
+    }
+  }
+
   return (
     <div className="h-screen w-screen flex flex-col">
+      <button onClick={insertTestData}> inserir dados de teste </button>
+      <button onClick={dropBaseData}> deletar dados de teste </button>
       <LogoContainer type={ETypeLogoContainer.bigNoReturn} />
       <div className="h-4/10 w-full bg-gradient-to-br from-blue-500 to-blue-400 flex flex-col sm:justify-evenly justify-center gap-15 sm:gap-0 items-center sm:flex-row">
         <div>
